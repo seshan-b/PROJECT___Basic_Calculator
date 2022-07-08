a7b829f0c36aba0693eb9b466bbd63e160a64701
seshan-b <seshan.b@gmail.com>
2022-07-08 17:54:29 +1200

    Edited JS file

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 
 
 
+
+
 add (1,3)
+
+divide (1,3)
+
+
+subtract (1,3)
+
+
+multiply (1,3)
+
 
 
 // Add 
@@ -15,7 +26,24 @@ function add(userInput1, userInput2) {
 
 // Divide
 
+function divide(userInput1, userInput2) {
+    console.log(userInput1 / userInput2 )
+    return userInput1 / userInput2
+    
+}
+
 // Subtract
 
+function subtract(userInput1, userInput2) {
+    console.log(userInput1 - userInput2 )
+    return userInput1 - userInput2
+    
+}
+
 // Multiply
+function multiply(userInput1, userInput2) {
+    console.log(userInput1 * userInput2 )
+    return userInput1 * userInput2
+    
+}
 
